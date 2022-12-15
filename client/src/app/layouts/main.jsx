6f853b4357ec131737cf9ecbfcaeb137c510487b
Pavel-Kn <ferrari.inside@gmail.com>
2022-12-15 12:26:01 +0300

    add login and register users

---
+++ client/src/app/layouts/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className="container mt-5">
+            <h1> Home </h1>
+        </div>
+    );
+};
+
+export default Main;
